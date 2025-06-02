fb6c7b956b48f697b29182c149f4a24cb984a470
nihaoxhr <2086677640@qq.com>
2025-06-02 20:01:14 +0800

    Add files via upload

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,421 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class 职员信息登记
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lv1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbTel = new System.Windows.Forms.TextBox();
+            this.tbDepartment = new System.Windows.Forms.TextBox();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.dTPJoinTime = new System.Windows.Forms.DateTimePicker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbCheck = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chGender = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chTel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chDepartment = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chJoinTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lv1
+            // 
+            this.lv1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chID,
+            this.chName,
+            this.chGender,
+            this.chTel,
+            this.chDepartment,
+            this.chJoinTime});
+            this.lv1.FullRowSelect = true;
+            this.lv1.GridLines = true;
+            this.lv1.HideSelection = false;
+            this.lv1.Location = new System.Drawing.Point(2, 3);
+            this.lv1.Name = "lv1";
+            this.lv1.Size = new System.Drawing.Size(776, 329);
+            this.lv1.TabIndex = 0;
+            this.lv1.UseCompatibleStateImageBehavior = false;
+            this.lv1.View = System.Windows.Forms.View.Details;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnChange);
+            this.groupBox1.Controls.Add(this.btnDel);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.dTPJoinTime);
+            this.groupBox1.Controls.Add(this.cbGender);
+            this.groupBox1.Controls.Add(this.tbDepartment);
+            this.groupBox1.Controls.Add(this.tbTel);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 338);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(455, 247);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "编辑操作";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbCheck);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.btnCheck);
+            this.groupBox2.Location = new System.Drawing.Point(12, 603);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 54);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "查询";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Controls.Add(this.monthCalendar1);
+            this.groupBox3.Location = new System.Drawing.Point(478, 341);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(290, 316);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "日历及提示";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "性别";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "入职时间";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "部门";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "电话";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "信息完整度";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(119, 18);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(121, 25);
+            this.tbName.TabIndex = 6;
+            // 
+            // tbTel
+            // 
+            this.tbTel.Location = new System.Drawing.Point(122, 88);
+            this.tbTel.Name = "tbTel";
+            this.tbTel.Size = new System.Drawing.Size(121, 25);
+            this.tbTel.TabIndex = 7;
+            // 
+            // tbDepartment
+            // 
+            this.tbDepartment.Location = new System.Drawing.Point(119, 124);
+            this.tbDepartment.Name = "tbDepartment";
+            this.tbDepartment.Size = new System.Drawing.Size(121, 25);
+            this.tbDepartment.TabIndex = 8;
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Items.AddRange(new object[] {
+            "男",
+            "女"});
+            this.cbGender.Location = new System.Drawing.Point(119, 54);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(59, 23);
+            this.cbGender.TabIndex = 9;
+            // 
+            // dTPJoinTime
+            // 
+            this.dTPJoinTime.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dTPJoinTime.Location = new System.Drawing.Point(122, 160);
+            this.dTPJoinTime.Name = "dTPJoinTime";
+            this.dTPJoinTime.Size = new System.Drawing.Size(118, 25);
+            this.dTPJoinTime.TabIndex = 10;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(119, 187);
+            this.progressBar1.Maximum = 5;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(286, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 11;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(323, 25);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "新增";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(323, 88);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 13;
+            this.btnDel.Text = "删除选定行";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(323, 56);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 14;
+            this.btnChange.Text = "修改";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(323, 126);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "清空记录";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(330, 16);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "查询";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 15);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "姓名";
+            // 
+            // tbCheck
+            // 
+            this.tbCheck.Location = new System.Drawing.Point(83, 17);
+            this.tbCheck.Name = "tbCheck";
+            this.tbCheck.Size = new System.Drawing.Size(176, 25);
+            this.tbCheck.TabIndex = 2;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 25);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 262);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(262, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // chID
+            // 
+            this.chID.Text = "序号";
+            this.chID.Width = 77;
+            // 
+            // chName
+            // 
+            this.chName.Text = "姓名";
+            this.chName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.chName.Width = 137;
+            // 
+            // chGender
+            // 
+            this.chGender.Text = "性别";
+            this.chGender.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.chGender.Width = 80;
+            // 
+            // chTel
+            // 
+            this.chTel.Text = "电话";
+            this.chTel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.chTel.Width = 147;
+            // 
+            // chDepartment
+            // 
+            this.chDepartment.Text = "部门";
+            this.chDepartment.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.chDepartment.Width = 94;
+            // 
+            // chJoinTime
+            // 
+            this.chJoinTime.Text = "入职时间";
+            this.chJoinTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.chJoinTime.Width = 232;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // 职员信息登记
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 685);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.lv1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "职员信息登记";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.职员信息登记_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.TextBox tbDepartment;
+        private System.Windows.Forms.TextBox tbTel;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DateTimePicker dTPJoinTime;
+        private System.Windows.Forms.TextBox tbCheck;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ColumnHeader chID;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chGender;
+        private System.Windows.Forms.ColumnHeader chTel;
+        private System.Windows.Forms.ColumnHeader chDepartment;
+        private System.Windows.Forms.ColumnHeader chJoinTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class 职员信息登记: Form
+    {
+        public 职员信息登记()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ListViewItem myLVI = lv1.Items.Add((lv1.Items.Count+1).ToString());
+            myLVI.SubItems.Add(tbName.Text);
+            myLVI.SubItems.Add(cbGender.Text);
+            myLVI.SubItems.Add(tbTel.Text);
+            myLVI.SubItems.Add(tbDepartment.Text);
+            myLVI.SubItems.Add(dTPJoinTime.Value.ToString("yyyy年MM月dd日"));
+            tbName.Clear();
+            tbDepartment.Clear();
+            cbGender.Text = "";
+            tbTel.Clear();
+            dTPJoinTime.Value = dTPJoinTime.MaxDate;
+        }
+
+        private void 职员信息登记_Load(object sender, EventArgs e)
+        {
+            dTPJoinTime.MaxDate = DateTime.Now;
+            dTPJoinTime.Value = dTPJoinTime.MaxDate;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = Image.FromFile(@"C:\Users\DELL\Desktop\新建文件夹\OIP-c.jpg");
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            if (lv1.SelectedItems.Count > 0)
+            {
+                lv1.SelectedItems[0].SubItems[1].Text = tbName.Text;
+                lv1.SelectedItems[0].SubItems[2].Text = cbGender.Text;
+                lv1.SelectedItems[0].SubItems[3].Text = tbTel.Text;
+                lv1.SelectedItems[0].SubItems[4].Text = tbDepartment.Text;
+                lv1.SelectedItems[0].SubItems[5].Text = dTPJoinTime.Value.ToString("yyyy年MM月dd日");
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (lv1.SelectedItems.Count > 0)
+            {
+                lv1.SelectedItems[0].Remove();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lv1.Items.Clear();
+        }
+        string[] path = System.IO.Directory.GetFiles(@"C:\Users\DELL\Desktop\新建文件夹\");
+        Random r = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(path[r.Next(0,path.Length)]);
+            int mum = 0;
+            if (tbName.Text!= "") mum++;
+            if (cbGender.Text != "") mum++;
+            if (tbTel.Text != "") mum++;
+            if (tbDepartment.Text != "") mum++;
+            if (dTPJoinTime.Value!=dTPJoinTime.MaxDate) mum++;
+            progressBar1.Value = mum;
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            ListViewItem checkItem = lv1.FindItemWithText(tbCheck.Text);
+            if (checkItem != null)
+            {
+                this.lv1.TopItem = checkItem;
+                checkItem.ForeColor = Color.Red;
+            }
+            else
+                MessageBox.Show("未找到该姓名");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
